6d9cd43d95535f2581a30d2fc69221e83bffadc1
Benedikt Böhm <bb@xnull.de>
2014-11-04 21:46:12 +0100

    add reachability helper

--- lib/druid/client.rb
+++ lib/druid/client.rb
@@ -1,3 +1,6 @@
+require 'timeout'
+require 'socket'
+
 require 'druid/zk'
 require 'druid/data_source'
 
@@ -10,9 +13,18 @@ module Druid
 
     def data_source(source)
       uri = @zk.data_sources[source]
-      return nil unless uri
+      return nil unless self.class.reachable?(uri)
       Druid::DataSource.new(source, uri)
     end
 
+    def self.reachable?(uri)
+      uri = URI(uri)
+      Timeout::timeout(1) do
+        TCPSocket.new(uri.host, uri.port).close
+      end
+      return true
+    rescue => e
+      return false
+    end
   end
 end
--- spec/lib/client_spec.rb
+++ spec/lib/client_spec.rb
@@ -11,10 +11,26 @@ describe Druid::Client do
       to_return(:status => 200, :body => "{\"dimensions\":[\"d1\", \"d2\"], \"metrics\":[\"m1\", \"m2\"]}", :headers => {})
     expect(Druid::ZK).to receive(:new).and_return(double(Druid::ZK, :data_sources => {'test/test' => 'http://www.example.com/druid/v2/'}, :close! => true))
     client = Druid::Client.new('test_uri')
+    expect(Druid::Client).to receive(:reachable?).and_return(true)
     ds = client.data_source('test/test')
     expect(ds.name).to eq('test/test')
     expect(ds.metrics).to eq(['m1', 'm2'])
     expect(ds.dimensions).to eq(['d1', 'd2'])
   end
 
+  context 'reachability' do
+
+    it 'unknown host' do
+      expect(Druid::Client.reachable?("http://kljhgfhcvbnmlkjh.com:1234")).to eq(false)
+    end
+
+    it 'closed port' do
+      expect(Druid::Client.reachable?("http://127.0.0.1:1/")).to eq(false)
+    end
+
+    it 'times out' do
+      expect(Druid::Client.reachable?("http://google.com:1")).to eq(false)
+    end
+
+  end
 end
