edca5961381cecf6740c3fc8e970149a7a37dfb0
Benedikt Böhm <bb@xnull.de>
2015-06-09 13:50:26 +0200

    format metadata in response correctly

--- lib/druid/console.rb
+++ lib/druid/console.rb
@@ -19,8 +19,17 @@ Ripl::Shell.class_eval do
 
   def _format_query_result(result, query)
     ap(query.as_json)
-    ap(result)
+    ap(result.as_json)
     return nil if result.empty?
+    case query.queryType
+    when 'timeseries', 'groupBy'
+      _format_timeseries_result(result, query)
+    when 'segmentMetadata'
+      _format_segment_metadata_result(result, query)
+    end
+  end
+
+  def _format_timeseries_result(result, query)
     result_key = _result_key(query.queryType)
     keys = result.last[result_key].keys
     grouped_result = result.group_by { |x| x['timestamp'] }
@@ -34,11 +43,21 @@ Ripl::Shell.class_eval do
     end
   end
 
+  def _format_segment_metadata_result(result, query)
+    columns = result.map do |row|
+      row['columns'].keys
+    end.to_a.flatten.uniq.sort
+    Terminal::Table.new(:headings => columns) do
+    end
+  end
+
   def format_result(result)
     if result.is_a?(Druid::Query::Builder)
       start = Time.now.to_f
-      puts _format_query_result($source.post(result.query), result.query)
-      puts "Response Time: #{(Time.now.to_f - start).round(3)}s"
+      response = $source.post(result.query)
+      rt = Time.now.to_f - start
+      puts _format_query_result(response, result.query)
+      puts "Response Time: #{rt.round(3)}s"
     else
       ap(result)
     end
@@ -77,22 +96,27 @@ module Druid
 
     def_delegators(*[
       :query,
+      :query_type,
+      :interval,
+      :granularity,
+      :metadata,
+      :timeseries,
       :group_by,
+      :topn,
+      :search,
+      :count,
       :sum,
       :long_sum,
       :double_sum,
-      :count,
-      :postagg,
-      :interval,
-      :granularity,
-      :filter,
-      :time_series,
-      :topn,
       :min,
       :max,
       :hyper_unique,
       :cardinality,
       :js_aggregation,
+      :postagg,
+      :filter,
+      :having,
+      :limit,
     ])
 
   end
